5195ea07b9c2bbb0ee59085e59f1630087f02d6d
Szn <144049463+ZYTROZ@users.noreply.github.com>
2026-09-05 15:32:22 -0400

    Update start.js

--- start.js
+++ start.js
@@ -4,23 +4,23 @@ console.log("======================================");
 console.log("           STARTING NOVI");
 console.log("======================================");
 
-const bot = spawn(process.execPath, ["index.js"], {
+const novi = spawn(process.execPath, ["index.js"], {
   stdio: "inherit",
   env: process.env,
 });
 
-bot.on("error", (err) => {
-  console.error("Failed to start index.js:");
+novi.on("error", (err) => {
+  console.error("Failed to start Novi:");
   console.error(err);
   process.exit(1);
 });
 
-bot.on("exit", (code, signal) => {
+novi.on("exit", (code, signal) => {
   if (signal) {
     console.log(`Novi stopped because of signal: ${signal}`);
-  } else {
-    console.log(`Novi exited with code: ${code}`);
+    process.exit(1);
   }
 
+  console.log(`Novi exited with code: ${code}`);
   process.exit(code ?? 1);
 });
